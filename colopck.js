5574cc722547508842d2ab44747ac213fd8d89bb
sandro gagoshidze <49387424+sannyybg@users.noreply.github.com>
2020-04-02 13:22:48 +0400

    Add files via upload

---
+++ colopck.js
@@ -0,0 +1,171 @@
+function redbutton() {
+    document.getElementById("name1").innerHTML = "NASTURCIAN FLOWER";
+    document.getElementById("div1").style.backgroundColor = "#e84118";
+    document.getElementById("emerald").innerHTML = "#e84118";
+
+    document.getElementById("name2").innerHTML = "ALIZARIN";
+    document.getElementById("div2").style.backgroundColor = "#e74c3c";
+    document.getElementById("wetasphalt").innerHTML = "#e74c3c";
+
+    document.getElementById("name3").innerHTML = "PASTEL RED";
+    document.getElementById("div3").style.backgroundColor = "#ff6b6b";
+    document.getElementById("carrot").innerHTML = "#ff6b6b";
+
+    document.getElementById("name4").innerHTML = "H. DAVIDSON ORANGE";
+    document.getElementById("div4").style.backgroundColor = "#c23616";
+    document.getElementById("alizarin").innerHTML = "#c23616";
+
+    document.getElementById("name5").innerHTML = "BARA RED";
+    document.getElementById("div5").style.backgroundColor = "#ED4C67";
+    document.getElementById("pumpkin").innerHTML = "#ED4C67";
+
+    document.getElementById("name6").innerHTML = "AMOUR";
+    document.getElementById("div6").style.backgroundColor = "#ee5253";
+    document.getElementById("peterriver").innerHTML = "#ee5253";
+
+    document.getElementById("name7").innerHTML = "POMEGRANATE";
+    document.getElementById("div7").style.backgroundColor = "#c0392b";
+    document.getElementById("sunflower").innerHTML = "#c0392b";
+
+    document.getElementById("name8").innerHTML = "RED PIGMENT";
+    document.getElementById("div8").style.backgroundColor = "#ea2027";
+    document.getElementById("concrete").innerHTML = "#ea2027";
+
+    document.getElementById("name9").innerHTML = "WILD WATERMELON";
+    document.getElementById("div9").style.backgroundColor = "#ff6b81";
+    document.getElementById("amethyst").innerHTML = "#ff6b81";
+
+    document.getElementById("name10").innerHTML = "BRUSCHETTA TOMATO";
+    document.getElementById("div10").style.backgroundColor = "#ff6348";
+    document.getElementById("turquoise").innerHTML = "#ff6348";
+}
+
+    function greenbutton() {
+    document.getElementById("name1").innerHTML = "ANDROID GREEN";
+    document.getElementById("div1").style.backgroundColor = "#A3CB38";
+    document.getElementById("emerald").innerHTML = "#A3CB38";
+
+    document.getElementById("name2").innerHTML = "PIXELATER GRASS";
+    document.getElementById("div2").style.backgroundColor = "#009432";
+    document.getElementById("wetasphalt").innerHTML = "#009432";
+
+    document.getElementById("name3").innerHTML = "ENERGOS";
+    document.getElementById("div3").style.backgroundColor = "#C4E538";
+    document.getElementById("carrot").innerHTML = "#C4E538";
+
+    document.getElementById("name4").innerHTML = "NEPHRITIS";
+    document.getElementById("div4").style.backgroundColor = "#27ae60";
+    document.getElementById("alizarin").innerHTML = "#27ae60";
+
+    document.getElementById("name5").innerHTML = "GREEN SEA";
+    document.getElementById("div5").style.backgroundColor = "#16a085";
+    document.getElementById("pumpkin").innerHTML = "#16a085";
+
+    document.getElementById("name6").innerHTML = "EMERALD";
+    document.getElementById("div6").style.backgroundColor = "#2ecc71";
+    document.getElementById("peterriver").innerHTML = "#2ecc71";
+
+    document.getElementById("name7").innerHTML = "DOWNLOAD PROGRESS";
+    document.getElementById("div7").style.backgroundColor = "#4cd137";
+    document.getElementById("sunflower").innerHTML = "#4cd137";
+
+    document.getElementById("name8").innerHTML = "LIME SOAP";
+    document.getElementById("div8").style.backgroundColor = "#7bed9f";
+    document.getElementById("concrete").innerHTML = "#7bed9f";
+
+    document.getElementById("name9").innerHTML = "UFO GREEN";
+    document.getElementById("div9").style.backgroundColor = "#2ed573";
+    document.getElementById("amethyst").innerHTML = "#2ed573";
+
+    document.getElementById("name10").innerHTML = "PARADISE GREEN";
+    document.getElementById("div10").style.backgroundColor = "#b8e994";
+    document.getElementById("turquoise").innerHTML = "#b8e994";
+
+}
+
+function yellowbutton() {
+    document.getElementById("name1").innerHTML = "SQUASHI BLOSSOM";
+    document.getElementById("div1").style.backgroundColor = "#f6b93b";
+    document.getElementById("emerald").innerHTML = "#f6b93b";
+
+    document.getElementById("name2").innerHTML = "PUFFINS BILL";
+    document.getElementById("div2").style.backgroundColor = "#EE5A24";
+    document.getElementById("wetasphalt").innerHTML = "#EE5A24";
+
+    document.getElementById("name3").innerHTML = "CARROT ORANGE";
+    document.getElementById("div3").style.backgroundColor = "#e58e26";
+    document.getElementById("carrot").innerHTML = "#e58e26";
+
+    document.getElementById("name4").innerHTML = "SOUR LEMON";
+    document.getElementById("div4").style.backgroundColor = "#ffeaa7";
+    document.getElementById("alizarin").innerHTML = "#ffeaa7";
+
+    document.getElementById("name5").innerHTML = "ORANGE";
+    document.getElementById("div5").style.backgroundColor = "#f39c12";
+    document.getElementById("pumpkin").innerHTML = "#f39c12";
+
+    document.getElementById("name6").innerHTML = "SUNFLOWER";
+    document.getElementById("div6").style.backgroundColor = "#FFC312";
+    document.getElementById("peterriver").innerHTML = "#FFC312";
+
+    document.getElementById("name7").innerHTML = "BRIGHT YARROW";
+    document.getElementById("div7").style.backgroundColor = "#fdcb6e";
+    document.getElementById("sunflower").innerHTML = "#fdcb6e";
+
+    document.getElementById("name8").innerHTML = "RISE-N-SHINE";
+    document.getElementById("div8").style.backgroundColor = "#fbc531";
+    document.getElementById("concrete").innerHTML = "#fbc531";
+
+    document.getElementById("name9").innerHTML = "FLAT FLESH";
+    document.getElementById("div9").style.backgroundColor = "#fad390";
+    document.getElementById("amethyst").innerHTML = "#fad390";
+
+    document.getElementById("name10").innerHTML = "ICELAND POPPY";
+    document.getElementById("div10").style.backgroundColor = "#fa983a";
+    document.getElementById("turquoise").innerHTML = "#fa983a";
+
+}
+
+function bluebutton() {
+    document.getElementById("name1").innerHTML = "BELIZZE HOLE";
+    document.getElementById("div1").style.backgroundColor = "#2980b9";
+    document.getElementById("emerald").innerHTML = "#2980b9";
+
+    document.getElementById("name2").innerHTML = "DEEP COVE";
+    document.getElementById("div2").style.backgroundColor = "#130f40";
+    document.getElementById("wetasphalt").innerHTML = "#130f40";
+
+    document.getElementById("name3").innerHTML = "MIDNIGHT BLUE";
+    document.getElementById("div3").style.backgroundColor = "#2c3e50";
+    document.getElementById("carrot").innerHTML = "#2c3e50";
+
+    document.getElementById("name4").innerHTML = "ELECTRON BLUE";
+    document.getElementById("div4").style.backgroundColor = "#0984e3";
+    document.getElementById("alizarin").innerHTML = "#0984e3";
+
+    document.getElementById("name5").innerHTML = "EXODUS FRUIT";
+    document.getElementById("div5").style.backgroundColor = "#686de0";
+    document.getElementById("pumpkin").innerHTML = "#686de0";
+
+    document.getElementById("name6").innerHTML = "BLURPLE";
+    document.getElementById("div6").style.backgroundColor = "#4834d4";
+    document.getElementById("peterriver").innerHTML = "#4834d4";
+
+    document.getElementById("name7").innerHTML = "SHY MOMENT";
+    document.getElementById("div7").style.backgroundColor = "#a29bfe";
+    document.getElementById("sunflower").innerHTML = "#a29bfe";
+
+    document.getElementById("name8").innerHTML = "DEEP KOAMARU";
+    document.getElementById("div8").style.backgroundColor = "#30336b";
+    document.getElementById("concrete").innerHTML = "#30336b";
+
+    document.getElementById("name9").innerHTML = "NASU PURPLE";
+    document.getElementById("div9").style.backgroundColor = "#5f27cd";
+    document.getElementById("amethyst").innerHTML = "#5f27cd";
+
+    document.getElementById("name10").innerHTML = "GREEN DARNER TAIL";
+    document.getElementById("div10").style.backgroundColor = "#74b9ff";
+    document.getElementById("turquoise").innerHTML = "#74b9ff";
+
+}
+
